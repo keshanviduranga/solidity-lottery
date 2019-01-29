6862890aa31af44eb902f0bcaf18d38588025112
elzed <elzed@users.noreply.github.com>
2019-01-29 11:19:16 -0800

    Add array and function to push players to Lottery
    Dynamic array created to hold players' addresses.
    Function added to push entries into that array.

--- Lottery.sol
+++ Lottery.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     function Lottery() public {
         manager = msg.sender;
+    }
+
+    function enter() public {
+        players.push(msg.sender);
     }
 }
