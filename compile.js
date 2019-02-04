35985204a8b1824ae6bf049174fdb8423d1342ad
elzed <elzed@users.noreply.github.com>
2019-02-04 15:10:38 -0800

    Update compile and deploy files for Lottery

--- compile.js
+++ compile.js
@@ -1,4 +1,4 @@
-// Needed to read Inbox Solidity file directly
+// Needed to read Lottery Solidity file directly
 // Cannot `require` the file because Node can't understand .sol
 
 const path = require('path'); // path may differ depending on OS - require path solves this
@@ -7,7 +7,7 @@ const fs = require('fs');
 const solc = require('solc');
 
 // __dirname set to current working directory
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
