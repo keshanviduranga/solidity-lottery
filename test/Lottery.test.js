59cbd49e3ce80eb939c81af3f539a72d38573b04
elzed <elzed@users.noreply.github.com>
2019-02-04 19:54:11 -0800

    Test Lottery.test for a minimum amount of ether

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -69,4 +69,16 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[2], players[2]);
         assert.equal(3, players.length);
     });
+
+    it('should require a minimum amount of ether to enter', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 200  // 200 wei
+            });
+            assert(false);  // used in case the test above does not fail
+        } catch (error) {
+            assert(error);
+        }
+    });
 });
